66038b994a4219765164828776168efc4476446f
ABHINAV SREEJITH <abhinavsree243@gmail.com>
2025-01-25 13:01:34 +0530

    Updated setup-db

--- backend/setup-db.js
+++ backend/setup-db.js
@@ -53,9 +53,4 @@ setupDatabase().catch(err => {
   process.exit(1);
 });
 
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_PORT:', process.env.DB_PORT);
 
